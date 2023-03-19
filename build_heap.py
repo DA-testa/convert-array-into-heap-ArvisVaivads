06838a731bf1965b0166e753f71f0bdca5f30340
Arvis Vaivads <124642194+ArvisVaivads@users.noreply.github.com>
2023-03-19 23:21:57 +0000

    BH Build_Heap

--- build_heap.py
+++ build_heap.py
@@ -1,47 +1,37 @@
-# Python3
-
-def parent(i):
-
-    return (i - 1) // 2
-
-def left_child(i):
-   
-    return 2 * i + 1
-
-def right_child(i):
-
-    return 2 * i + 2
-
-
-def sift_down(data, i, swaps):
-   
-    min_index = i
-    l = left_child(i)
-
-    if l < len(data) and data[l] < data[min_index]:
-        min_index = l
-    r = right_child(i)
-
-    if r < len(data) and data[r] < data[min_index]:
-        min_index = r
-
-    if i != min_index:
-        data[i], data[min_index] = data[min_index], data[i]
-        swaps.append((i, min_index))
-        sift_down(data, min_index, swaps)
-
 def build_heap(data):
     swaps = []
     for i in range(len(data) // 2, -1, -1):
-        sift_down(data, i, swaps)
+        min_index = i
+        l = 2 * i + 1
+        r = 2 * i + 2
+        if l < len(data) and data[l] < data[min_index]:
+            min_index = l
+        if r < len(data) and data[r] < data[min_index]:
+            min_index = r
+        if i != min_index:
+            data[i], data[min_index] = data[min_index], data[i]
+            swaps.append((i, min_index))
+            j = min_index
+            while j < len(data):
+                l = 2 * j + 1
+                r = 2 * j + 2
+                if l < len(data) and data[l] < data[j]:
+                    data[l], data[j] = data[j], data[l]
+                    swaps.append((l, j))
+                    j = l
+                elif r < len(data) and data[r] < data[j]:
+                    data[r], data[j] = data[j], data[r]
+                    swaps.append((r, j))
+                    j = r
+                else:
+                    break
     return swaps
 
 def main():
-
     input_source = input("I or F")
     if input_source == 'F':
         file_name = input("File name: ")
-        with open( "tests/" + file_name, 'r', encoding="utf-8") as f:
+        with open(file_name, 'r', encoding="utf-8") as f:
             n = int(f.readline().strip())
             data = list(map(int, f.readline().strip().split()))
     else:
