142d320e1ad5799c7062680380508652efe9d577
Arvis Vaivads <124642194+ArvisVaivads@users.noreply.github.com>
2023-03-20 01:03:00 +0000

    HEAP 2 2

--- build_heap.py
+++ build_heap.py
@@ -1,49 +1,48 @@
+# python3
+
 def build_heap(data):
     swaps = []
-    for i in range(len(data) // 2, -1, -1):
+
+    def sift_down(i):
         min_index = i
-        l = 2 * i + 1
-        r = 2 * i + 2
-        if l < len(data) and data[l] < data[min_index]:
-            min_index = l
-        if r < len(data) and data[r] < data[min_index]:
-            min_index = r
+        left_child = 2 * i + 1
+        if left_child < len(data) and data[left_child] < data[min_index]:
+            min_index = left_child
+        right_child = 2 * i + 2
+        if right_child < len(data) and data[right_child] < data[min_index]:
+            min_index = right_child
         if i != min_index:
             data[i], data[min_index] = data[min_index], data[i]
             swaps.append((i, min_index))
-            j = min_index
-            while j < len(data):
-                l = 2 * j + 1
-                r = 2 * j + 2
-                if l < len(data) and data[l] < data[j]:
-                    data[l], data[j] = data[j], data[l]
-                    swaps.append((l, j))
-                    j = l
-                elif r < len(data) and data[r] < data[j]:
-                    data[r], data[j] = data[j], data[r]
-                    swaps.append((r, j))
-                    j = r
-                else:
-                    break
+            sift_down(min_index)
+
+    for i in range(len(data) // 2, -1, -1):
+        sift_down(i)
+
     return swaps
 
 def main():
-    input_source = input("I or F")
-    if input_source == 'F':
-        file_name = input("File name: ")
-        with open(file_name, 'r', encoding="utf-8") as f:
-            n = int(f.readline().strip())
-            data = list(map(int, f.readline().strip().split()))
-    else:
+
+    input_type = input("I or F")
+
+    if input_type.upper() == 'I':
         n = int(input())
         data = list(map(int, input().split()))
-        assert len(data) == n
+    elif input_type.upper() == 'F':
+        file_name = input("File name: ")
+        with open("tests/" + file_name, 'r', encoding="utf-8") as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+    else:
+        print("Invalid input type")
+        return
 
+    assert len(data) == n
     swaps = build_heap(data)
-
-    print(len(swaps), end=" ")
+    print(len(swaps))
     for i, j in swaps:
-        print(i, j, end=" ")
+        print(i, j)
+
 
 if __name__ == "__main__":
     main()
